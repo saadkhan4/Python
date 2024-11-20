9cd6fc0e69090e4b9ef4212d0e8d331e857dd400
saadkhan4 <saadshaheen1327@gmail.com>
2024-11-20 22:35:58 +0500

    z

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# from cs50 import get_int
+
+# x = get_int("x: ")
+# y = get_int("y: ")
+
+# print(x + y)
+
+# with the help of cs50 library
+
+y = int(input("y: "))
+z = int(input("z: "))
+
+print(y + z)
+
+# without help of cs50 library we use int & input so the integers can behave as int.
+
+
+
--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
-print("hello, world123")
+
+answer = input("What's your name? ")
+print("hello, " + answer)
