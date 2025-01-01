438f438f176b39742c18398b08263733059914eb
saadkhan4 <saadshaheen1327@gmail.com>
2025-01-01 21:59:09 +0500

    practice of python functions

--- function.py
+++ function.py
@@ -4,52 +4,81 @@
 
 
 
-def special():
-    special = 1
-    while special < 4:
-        special += 1
-        print("hello form universe")
+# def special():
+#     special = 1
+#     while special < 4:
+#         special += 1
+#         print("hello form universe")
 
 
-special()
-
-
-
-
-def people():
-    people = 1
-    while people < 3:
-        people += 1
-        print("hello people")
-
-
-people()
+# special()
 
 
 
-def house():
-    for house in range(3):
-        print("in order wise")
+# def people():
+#     people = 1
+#     while people < 4:
+#         people += 1
+#         print("free the  people")
 
 
-house()        
-
-
-
-answer = input("what's your name? ")
-print(f"hello , {answer}")
+# people()
 
 
 
+def orange():
+    for orange in range(3):
+        print("it taste better")
 
 
-def ribbon():
-    ribbon = 0
-    while ribbon < 3:
-        ribbon += 1
-    print("hello , world")
+
+orange()        
 
 
-ribbon()    
+# def people():
+#     people = 1
+#     while people < 3:
+#         people += 1
+#         print("hello people")
 
 
+# people()
+
+
+
+# def house():
+#     for house in range(3):
+#         print("in order wise")
+
+
+# house()        
+
+
+
+# answer = input("what's your name? ")
+# print(f"hello , {answer}")
+
+
+
+
+
+# def ribbon():
+#     ribbon = 0
+#     while ribbon < 3:
+#         ribbon += 1
+#     print("hello , world")
+
+
+# ribbon()    
+
+
+
+
+# def dog():
+#     dog = 0
+#     while dog < 3:
+#         dog += 1
+#     print("he is in the yard")
+
+
+# dog()    
--- practice.py
+++ practice.py
@@ -1,20 +1,32 @@
 from cs50 import get_string , get_int
 
-# answer = get_string("what's your name? ")
-# print(f"hello , {answer}")
+answer = get_string("what's your name? ")
+print(f"hello , {answer}")
+
+
 
 # answer = input("what's your name? ")
 # print(f"hello , {answer}")
 
-# t = get_int("x: ")
-# y = get_int("y: ")
 
-# print(t + y)
 
-# u = int(input("u: "))
-# p = int(input("p: "))
+y = get_int("y: ")
+g = get_int("g: ")
 
-# print(u + p)
+
+print(y + g)
+
+u = int(input("u: "))
+p = int(input("p: "))
+
+print(u + p)
+
+
+y = int(input("y: "))
+e = int(input("e: "))
+
+
+print(y + e)
 
 # q = int(input("q: "))
 # y = int(input("y: "))
@@ -22,24 +34,34 @@ from cs50 import get_string , get_int
 # b = (q / y)
 # print(f"{b:.50f}")
 
-# x = get_int("what's x? ")
-# y = get_int("what's y? ")
+e = get_int("what's e? ")
+f = get_int("what's f? ")
 
-# if (x < y):
-#     print("x is less than y")
+if (e < f):
+    print("e is less than f")
 
-# elif (x > y):
-#     print("x is greater than y")
+elif (e > f):
+    print("e is greater than f")
 
-# else:
-#     print("x is equal to y")
-
-for x in range(3):
-    print(x)
 else:
-    print("finally completed")    
+    print("e is equal to y")
+
+    
 
 
 
-answer = get_string("what's your name? ")
-print(f"hello , {answer}")
+
+
+
+
+
+
+# for x in range(3):
+#     print(x)
+# else:
+#     print("finally completed")    
+
+
+
+# answer = get_string("what's your name? ")
+# print(f"hello , {answer}")
