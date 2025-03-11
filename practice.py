47cc13ee2382dafa4117752f9ac9bc41ecbeb77b
saadkhan4 <saadshaheen1327@gmail.com>
2025-03-11 22:16:24 +0500

    practice of different functions

--- practice.py
+++ practice.py
@@ -1,212 +1,161 @@
 from cs50 import get_string , get_int
 
-# answer = input("what's your name? ")
-# print(f"hello ,{answer}")
+answer = get_string("what's your name? ")
+print(f"hello ,{answer}")
 
-# answer = get_string("what's your name? ")
-# print(f"hello ,{answer}")
+answer = input("what's your name? ")
+print(f"hello ,{answer}")
 
-# v = get_int("v: ")
-# t = get_int("t: ")
+c = get_int("c: ")
+p = get_int("p: ")
 
-# print(v + t)
+print(c + p)
 
-# c = int(input("c: "))
-# p = int(input("p: "))
+v = int(input("v: "))
+k = int(input("k: "))
 
-# print(c - p)
+print(v - k)
 
-# x = int(input("x: "))
-# q = int(input("q: "))
+t = int(input("t: "))
+o = int(input("o: "))
 
-# print(x * q)
+b = t / o
+print(b)
 
+q = int(input("q: "))
+i = int(input("i: "))
 
-# r = int(input("r: "))
-# w = int(input("w: "))
+print(q * i)
 
-# print(r / w)
+y = int(input("y: "))
+p = int(input("p: "))
 
-# q = int(input("q: "))
-# k = int(input("k: "))
+g = y / p
+print(f"{g:.43f}")
 
-# r = q / k
-# print(r)
+l = int(input("l: "))
+u = int(input("u: "))
 
+if (l < u):
+    print("l is less than u")
 
-# d = int(input("d: "))
-# l = int(input("l: "))
+elif(l > u):
+    print("l is greater than u")
 
-# g = d / l
-# print(f"{g:.43f}")
-
-# k = int(input("k: "))
-# o = int(input("o: "))
-
-# if (k < o):
-#     print("k is less than o")
-
-# elif (k > o):
-#     print("k is greater than o")
-
-# else:
-#     print("k and o are equal")
-
-            
-# s = input("s: ")
-# p = input("p: ")
-
-# if s == p:
-#     print("same")
-
-# else: 
-#     print("different") 
+else:
+    print("l and u are equal")    
 
 
+f = input("f: ")
+l = input("l: ")
 
-# names = ["Ali","Megan","Cassandra"]
-# name = input("name: ")
+if (f == l):
+    print("same")
 
-# if name in names:
-#     print("found")
+else:
+    print("different")
 
-# else:
-#     print("not found")
+names = ["Megan","Dusty","Charlotte"]
+name = input("Name: ")
+if name in names:
+    print("found")
+else:
+    print("not found")
 
+scores = []
+for i in range(3):
+    player = int(input("Score: "))
+    scores.append(player)
 
-# scores = []
-# for i in range(3):
-#     player = int(input("score: "))
-#     scores.append(player)
+average = sum(scores) / len(scores)
+print(f"Average: {average}")    
 
-# average = sum(scores) / len(scores)
-# print(f"Average: {average}")   
-
-   
-   
-# def system():
-#     print("system is malfunctioning")
-
-
-# system()
-
-
-# def sort_array():
-#     for i in range(4):
-#         print("array has been started")
+def system():
+    print("system is working")
 
 
-# sort_array()
+system()
+
+def sys_loop():
+    while True:
+        print("loop begins from here")
+
+sys_loop()
+
+def machine():
+    for i in range(3):
+        print("machine is malfunctioning")
+
+machine()
 
 
-# def delete_file():
-#     while True:
-#         print("file has been deleted")
+def sys_loop():
+    sys_loop = 0
+    while sys_loop < 3:
+        sys_loop += 1 
+        print("loop begins from here")
+
+sys_loop()        
+
+def greet(name):
+    print(f"hello , {name}")
+
+greet("Natasha")    
+
+def get_sys_info():
+    for i in range(3):
+        machine()
 
 
-# delete_file()
+def machine():
+    print("info is confidential")
 
 
+get_sys_info()
 
-# def sys_loop():
-#     sys_loop = 0
-#     while sys_loop < 3:
-#         sys_loop += 1
-#         print("loop start from here")
-
-
-# sys_loop()        
-
-
-# def greet(name):
-#     print(f"hello , {name}")
-
-
-# greet("Natasha")
-
-# def main():
-#     for i in range(3):
-#         sys_loop()
-
-
-
-# def sys_loop():
-#     print("loop has been initialized") 
-
-
-
-# main()          
-
-# def odd_or_even(number):
-#     if number % 2 == 0:
-#         return "Even"
-#     else:
-#         return "Odd"
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
     
-
-# num = int(input("Enter your number: "))
-# print(f"The num {num} is {odd_or_even(num)}")
-
-# def reverse_string(input_string):
-#     return input_string [::-1]
-
-# user_input = input("Enter your string: ")
-# print(f"The reverse string is {reverse_string(user_input)}")
+num = int(input("Enter your number: "))
+print(f"The num {num} is {odd_or_even(num)}")
 
 
-# def second_largest(numbers):
-#     unique_number = sorted(set(numbers),reverse=True)
-#     return unique_number[1]
+def reverse_string(input_string):
+    return input_string[::-1]
+
+user_input = input("Enter your string: ")
+print(f"The reverse string is {reverse_string(user_input)}")
 
 
-# num_list = [34,34,56,76,88,98,12,54]
-# print(f"The second largest number is {second_largest(num_list)}")
+def second_largest(numbers):
+    unique_number = sorted(set(numbers),reverse=True)
+    return unique_number[1]
 
-# def find_duplicates(numbers):
-#     duplicates = []
-#     seen = set()
+num_list = [23,44,123,33,43,123,43,56,77]
+print(f"The second largest number is {second_largest(num_list)}")
 
-#     for num in numbers:
-#         if num in seen and num not in duplicates:
-#             duplicates.append(num)
-#         seen.add(num)
+def find_duplicates(numbers):
+    duplicates = []
+    seen = set()
 
-#     return duplicates
+    for num in numbers:
+        if num in seen and num not in duplicates:
+            duplicates.append(num)
+        seen.add(num)
 
-# num_list = [1,4,2,5,6,7,8,9,3,4,5,1,7,2,9,4]
-# print(f"Duplicate numbers: {find_duplicates(num_list)}")
+    return duplicates
 
-
+num_list = [1,4,5,12,4,465,8,23,2,3,4,6,8,9,3,5,1,56]
+print(f"Duplicate numbers: {find_duplicates(num_list)}")        
 
 def are_anagrams(str1, str2):
     str1 = str1.replace(" ", "").lower()
-    str2 = str2.replace(" ", "").lower()
+    str2 = str2.replace(" ","").lower()
 
     return sorted(str1) == sorted(str2)
 
-print(are_anagrams("Listen", "Silent"))
-print(are_anagrams("Hello", "World"))
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
+print(are_anagrams("Listen" , "Silent"))
+print(are_anagrams("Greet", "Hello"))
--- problems.py
+++ problems.py
@@ -1,48 +1,48 @@
 # # QUESTION: Write a Python function to check if a given number is odd or even.
-# def odd_or_even(number):
-#     if number % 2 == 0:             #This checks if the number is divisible by 2 without a remainder.
-#         return "Even"
-#     else:
-#         return "Odd"
+def odd_or_even(number):
+    if number % 2 == 0:             #This checks if the number is divisible by 2 without a remainder.
+        return "Even"
+    else:
+        return "Odd"
     
-# num = int(input("Enter a number: "))
-# print(f"The number {num} is {odd_or_even(num)}")
+num = int(input("Enter a number: "))
+print(f"The number {num} is {odd_or_even(num)}")
 
-# # we used % sign because it will divide the number without giving remainder.
-# # if we used / (divide) sign, it will give us the remainder or decimal.
+# we used % sign because it will divide the number without giving remainder.
+# if we used / (divide) sign, it will give us the remainder or decimal.
 
 
-# # QUESTION: Write a Python function that takes a string as input and returns the string reversed.
-# def reverse_string(input_string):
-#     return input_string[::-1]       #[::-1] means “take the entire string but step backwards by 1”.
+# QUESTION: Write a Python function that takes a string as input and returns the string reversed.
+def reverse_string(input_string):
+    return input_string[::-1]       #[::-1] means “take the entire string but step backwards by 1”.
 
-# user_input = input("Enter a string: ")
-# print(f"The reverse string is {reverse_string(user_input)}")
+user_input = input("Enter a string: ")
+print(f"The reverse string is {reverse_string(user_input)}")
 
-#QUESTION: Write a Python function to find the second largest number in a list. Assume the list contains at least two unique numbers.
-# def second_largest(numbers):
-#     unique_number = sorted(set(numbers), reverse=True)   #	set(numbers) converts the list to a set, which removes duplicate values. ###sorted(..., reverse=True) sorts the unique numbers in descending order (largest to smallest).
-#     return unique_number[1]                              #  unique_numbers[1] retrieves the second element from the sorted list.
+# QUESTION: Write a Python function to find the second largest number in a list. Assume the list contains at least two unique numbers.
+def second_largest(numbers):
+    unique_number = sorted(set(numbers), reverse=True)   #	set(numbers) converts the list to a set, which removes duplicate values. ###sorted(..., reverse=True) sorts the unique numbers in descending order (largest to smallest).
+    return unique_number[1]                              #  unique_numbers[1] retrieves the second element from the sorted list.
 
-# num_list = [10, 20, 4, 45, 99, 99] 
-# print(f"The second largest number is {second_largest(num_list)}")
+num_list = [10, 20, 4, 45, 99, 99] 
+print(f"The second largest number is {second_largest(num_list)}")
 
-#QUESTION:-Data Structures:
+# QUESTION:-Data Structures:
 # You are given a list of integers where some numbers appear more than once. Write a Python function to return a list of duplicates.
-# def find_duplicate(numbers):
-#     duplicates = []       #Stores duplicate numbers.
-#     seen = set()          #Tracks numbers that have already appeared.
+def find_duplicate(numbers):
+    duplicates = []       #Stores duplicate numbers.
+    seen = set()          #Tracks numbers that have already appeared.
 
-#     for num in numbers:
-#         if num in seen and num not in duplicates:    #num not in duplicates → Ensures we don’t add the same duplicate multiple times.
-#             duplicates.append(num)
-#         seen.add(num)     #stores numbers that have appeared.
+    for num in numbers:
+        if num in seen and num not in duplicates:    #num not in duplicates → Ensures we don’t add the same duplicate multiple times.
+            duplicates.append(num)
+        seen.add(num)     #stores numbers that have appeared.
 
-#     return duplicates
+    return duplicates
 
-# num_list = [1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 3]
-# print(f"Duplicate numbers: {find_duplicate(num_list)}")
-#QUESTION:-
+num_list = [1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 3]
+print(f"Duplicate numbers: {find_duplicate(num_list)}")
+# QUESTION:-
 # Logical Problem:
 # Write a Python function to check if two strings are anagrams of each other. Ignore spaces and capitalization.             
 def are_anagrams(str1, str2):
@@ -57,15 +57,6 @@ def are_anagrams(str1, str2):
 print(are_anagrams("Listen", "Silent"))  # Output: True
 print(are_anagrams("Hello", "World"))    # Output: False
 
-
-# def are_anagrams(str1, str2):
-#     str1 = str1.replace(" ", "").lower
-#     str2 = str2.replace(" ", "").lower
-
-#     return sorted(str1) == sorted(str2)
-
-# print(are_anagrams("Listen", "Silent"))
-# print(are_anagrams("Hello", "World"))
 
 
 
