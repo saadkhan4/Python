d3add8dea082bb31f13a8a2d2c59e341b857a2c6
saadkhan4 <saadshaheen1327@gmail.com>
2025-04-21 22:39:30 +0500

    made a consession program

---
+++ Python-course/concession.py
@@ -0,0 +1,32 @@
+# concession stand program
+
+menu = {
+    "soda": 1.50,
+    "water": 1.25,
+    "chips": 2.00,
+    "candy": 1.75,
+    "cookie": 1.00
+}
+
+cart = []
+total = 0
+
+print("----- MENU -----")
+for key, value in menu.items():
+    print(f"{key:8}: {value:.2f}")
+
+while True:
+    food = input("Select an item to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+    
+print("----- YOUR ORDER-----")
+for food in cart:
+    total += menu.get(food)
+    print(food, end=" ")
+
+print()
+print(f"Total is: ${total:.2f}")    
+
--- Python-course/shopping-cart.py
+++ Python-course/shopping-cart.py
@@ -43,21 +43,24 @@ while True:
     food = input("Enter a food to buy (q to quit): ")
     if food.lower() == "q":
         break
-
-    price = float(input(f"Enter the price of {food}: $"))
-    foods.append(food)
-    prices.append(price)
+    else:
+     price = float(input(f"Enter the price of {food}: $"))
+     foods.append(food)
+     prices.append(price)
 
 print("----- YOUR CART -----")
 
 for food in foods:
-    print(food, end=" ")
+   print(food, end=" ")
 
 for price in prices:
-    total += price
+   total += price
 
 print()
-print(f"Your total is ${total}")        
+print(f"Your total is ${total}")      
+
+
+
 
 
 
